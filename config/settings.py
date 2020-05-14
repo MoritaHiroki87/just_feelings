2bde49abbe23bbdba7a1955ea6962fbcc5ef9419
森田寛樹 <>
2020-05-14 08:19:32 -0400

    update: allauth導入が一旦終わりかなという感じ

--- config/settings.py
+++ config/settings.py
@@ -156,10 +156,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
@@ -169,4 +167,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 ###############
 SITE_ID = 1
 LOGIN_REDIRECT_URL = 'main:home'
-ACCOUNT_LOGOUT_REDIRECT_URL = '/account/login/'
+ACCOUNT_LOGOUT_REDIRECT_URL = '/accounts/login/'
--- main/views.py
+++ main/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 
 class MainView(TemplateView):
     template_name = 'main/home.html'
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
